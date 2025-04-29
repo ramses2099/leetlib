b0cad24b2752da0702d7285546b16115693f0a0a
ramses2099 <ramses2099@gmail.com>
2025-04-29 09:24:20 -0400

    1365. How Many Numbers Are Smaller Than the Current Number

---
+++ src/lc_1365_small_than_the_current_number.rs
@@ -0,0 +1,42 @@
+#![allow(dead_code)]
+use std::collections::HashMap;
+
+pub fn smaller_numbers_than_current(nums: Vec<i32>) -> Vec<i32> {
+    let mut rs: Vec<i32> = Vec::new();
+    let mut map = HashMap::new();
+    let mut temp = nums.clone();
+    temp.sort();
+
+    for (i, v) in temp.iter().enumerate() {
+        if map.contains_key(v) {
+            continue;
+        }
+        map.insert(v, i);
+    }
+
+    for i in nums.iter() {
+        rs.push(map[i] as i32);
+    }
+    rs
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ex1() {
+        let nums = vec![8, 1, 2, 2, 3];
+        let outpt = vec![4, 0, 1, 1, 3];
+
+        assert_eq!(smaller_numbers_than_current(nums), outpt);
+    }
+
+    #[test]
+    fn ex2() {
+        let nums = vec![7, 7, 7, 7];
+        let outpt = vec![0, 0, 0, 0];
+
+        assert_eq!(smaller_numbers_than_current(nums), outpt);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ mod lc_00217_contains_duplicate;
 mod lc_00268_missing_number;
 mod lc_00448_find_all_number_disappeared;
 mod lc_242_valid_anagram;
+mod lc_1365_small_than_the_current_number;
 
 use lc_0001_two_sum::*;
 use lc_0020_valid_parentheses::*;
@@ -12,3 +13,4 @@ use lc_00217_contains_duplicate::*;
 use lc_00268_missing_number::*;
 use lc_00448_find_all_number_disappeared::*;
 use lc_242_valid_anagram::*;
+use lc_1365_small_than_the_current_number::*;
